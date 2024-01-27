5df2ac88c0e2645a21c5bd65fafa0fd96831b9b9
youssef-elmelegy <youssefelmelegy999@gmail.com>
2024-01-27 14:43:18 +0200

    main.c

---
+++ baby.h
@@ -0,0 +1,8 @@
+#ifndef BABY_H
+#define BABY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#endif
--- main.c
+++ main.c
@@ -1,1 +1,21 @@
 // main.c
+#include "shell.h"
+
+int main(int argc, char **argv)
+{
+        (void) argc, argv;
+        char *prom = "baby_shell>>";
+        size_t length = 0;
+        ssize_t count = 0;
+
+
+        while (1)
+        {
+                printf("%s", prom);
+                count = getline(&input, &length, stdin);
+                
+        }
+
+
+        return (0);
+}
